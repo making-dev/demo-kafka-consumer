1b36b06c1068e8660d58830a4129b44baa25b47c
Toshiaki Maki <makingx@gmail.com>
2024-07-17 14:21:33 +0900

    Add test

--- src/test/java/com/example/DemoKafkaConsumerApplicationTests.java
+++ src/test/java/com/example/DemoKafkaConsumerApplicationTests.java
@@ -1,15 +1,52 @@
 package com.example;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.client.RestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoKafkaConsumerApplicationTests {
+	@Autowired RestClient.Builder restClientBuildr;
+
+	@LocalServerPort int port;
+
+	RestClient restClient;
+
+	@Autowired
+	KafkaTemplate<String, String> kafkaTemplate;
+
+	@Value("${demo.topic}") String topic;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		this.restClient = this.restClientBuildr
+				.baseUrl("http://localhost:%d".formatted(port))
+				.build();
+	}
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		this.kafkaTemplate.send(this.topic, "Hello World!").get(3, TimeUnit.SECONDS);
+		List<String> body = this.restClient.get()
+				.uri("/")
+				.retrieve()
+				.body(new ParameterizedTypeReference<>() {
+				});
+		assertThat(body).containsExactly("Hello World!");
 	}
 
 }
